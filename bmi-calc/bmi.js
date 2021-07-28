37bd771b979471beb3b5a07fce4ef96180a7fb64
KushalYagnik <kushalyagnik@gmail.com>
2021-07-28 13:08:09 -0400

    Added color changes depending on BMI

--- bmi-calc/bmi.js
+++ bmi-calc/bmi.js
@@ -11,7 +11,14 @@ function calcBMI() {
 
         bmi = kg / Math.pow(newCM, 2);
         bmi = bmi.toFixed(1);
-        console.log(bmi);
+
+        if (bmi <= 18.6) {
+            document.getElementById("result").style.color = "yellow";
+        } else if (bmi >= 18.6 && bmi <= 24.9) {
+            document.getElementById("result").style.color = "rgb(179, 255, 0)";
+        } else {
+            document.getElementById("result").style.color = "orange"
+        }
 
         document.getElementById("result").innerHTML = `Your BMI is ${bmi}`;
     }
