6fbb1b0085232fa6cdc78c72d593ef2cda1968ba
KushalYagnik <kushalyagnik@gmail.com>
2021-08-01 09:04:07 -0400

    Output number fixed to 2 decimals

--- weight-conv/weight-conv.js
+++ weight-conv/weight-conv.js
@@ -1,5 +1,5 @@
 function weightConverter(valNum) {
     valNum = valNum * 2.2046;
-    let n = valNum.toFixed(0);
+    let n = valNum.toFixed(2);
     document.getElementById("outputLbs").innerHTML = n;
 }
