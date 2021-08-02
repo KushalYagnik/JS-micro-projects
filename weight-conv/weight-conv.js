68f59be107e1b64c51039d1acfb310da6fa1f48b
KushalYagnik <kushalyagnik@gmail.com>
2021-08-02 17:28:51 -0400

    Added functionality to reverse units

--- weight-conv/weight-conv.js
+++ weight-conv/weight-conv.js
@@ -1,5 +1,35 @@
+let currentUnit = 'kg';
+
 function weightConverter(valNum) {
-    valNum = valNum * 2.2046;
-    let n = valNum.toFixed(2);
-    document.getElementById("outputLbs").innerHTML = n;
+    if (currentUnit === 'kg') {
+        valNum = valNum * 2.2046;
+        let n = valNum.toFixed(2);
+        document.getElementById("outputWgt").innerHTML = n;
+    }
+    else {
+        valNum = valNum / 2.2046;
+        let n = valNum.toFixed(2);
+        document.getElementById("outputWgt").innerHTML = n;
+    }
+}
+
+function reverse() {
+    if (currentUnit === 'kg') {
+        document.getElementById("h1").innerHTML = "Weight Converter (Lbs to Kg)";
+        document.getElementById("unit1").innerHTML = "Lbs";
+        document.getElementById("inputWgt").setAttribute("placeholder", "Pounds");
+        document.getElementById("unit2").innerHTML = "Kg";
+        currentUnit = 'lbs';
+    }
+    else {
+        // else if (currentUnit === 'lbs') {
+        document.getElementById("h1").innerHTML = "Weight Converter (Kg to Lbs)";
+        document.getElementById("unit1").innerHTML = "Kg";
+        document.getElementById("inputWgt").setAttribute("placeholder", "Kilograms");
+        document.getElementById("unit2").innerHTML = "Lbs";
+        currentUnit = 'kg';
+    }
+    document.getElementById("inputWgt").value = '';
+    document.getElementById("outputWgt").innerHTML = '';
+    console.log(currentUnit);
 }
