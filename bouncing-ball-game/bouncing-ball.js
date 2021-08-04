bc51c918f0ce3c4701354af85e0af5012cdd53c7
KushalYagnik <kushalyagnik@gmail.com>
2021-08-04 14:38:20 -0400

    Separating the draw functions

--- bouncing-ball-game/bouncing-ball.js
+++ bouncing-ball-game/bouncing-ball.js
@@ -4,13 +4,22 @@ let ctx = canvas.getContext("2d");
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 
+let dx = 2;
+let dy = -2;
 
-function draw() {
+function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, 10, 0, Math.PI * 2);
     ctx.fillStyle = "whitesmoke";
     ctx.fill();
     ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canas.height);
+    drawBall();
+    x += dx;
+    y += dy;
 }
 
 setInterval(draw, 10)
