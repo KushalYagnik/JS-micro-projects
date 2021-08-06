791890be20a7cea82d923b4d052d15f4d7a11d89
KushalYagnik <kushalyagnik@gmail.com>
2021-08-06 08:34:28 -0400

    Added function to draw the paddle.

--- bouncing-ball-game/bouncing-ball.js
+++ bouncing-ball-game/bouncing-ball.js
@@ -6,13 +6,29 @@ let y = canvas.height - 30;
 
 let dx = 2;
 let dy = -2;
-
 let ballRadius = 10;
+
+let paddleHeight = 12;
+let paddleWidth = 72;
+let paddleX = (canvas.width - paddleWidth) / 2;
+let rightMove = false;
+let leftMove = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "whitesmoke";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "orangered";
     ctx.fill();
     ctx.closePath();
 }
@@ -28,6 +44,7 @@ function draw() {
     }
     x += dx;
     y += dy;
+    drawPaddle();
 }
 
 setInterval(draw, 10)
